994dbaea7e838826719454336a4a69e76beec4ca
Alan Diek Guimarães <alandiekguimaraes@gmail.com>
2024-07-06 23:47:32 -0300

    Atualizando o código.

--- snake.py
+++ snake.py
@@ -10,12 +10,22 @@ preto = (0, 0, 0)                                                   # Cores em R
 screen.fill(preto)                                                  # Definindo a cor da tela como preta
 pygame.display.update()                                             # Atualizando a tela para aplicar a cor definida.
 
+
+class Snake:                                                        # Classe que define a cobra no jogo.
+    cor = (255, 255, 255)                                           # Cor branca (em RGB) para a cobra.
+    tamanho = (10, 10)                                              # Tamanho de cada segmento da cobra.
+
+    def __init__(self):                                             # Método inicializador da classe.
+        self.textura = pygame.Surface(self.tamanho)                 # Criando uma superfície para cada pixel da cobra.
+        self.textura.fill(self.cor)                                 # Preenchendo a superfície com a cor branca.
+
+        self.corpo = [(100, 100),(90, 100), (80, 100) ]             # Lista que define a posição inicial da cobra.
+
+
 class Frutinha:                                                     # Definindo a frutinha no jogo.
     cor = (255, 0, 0)                                               # Atribuindo a cor vermelha (em RGB) a a variavel cor.
     tamanho = (10, 10)                                              # Definindo o tamanho para a frutinha.
-
-
-    def __init__(self):
+    def __init__(self):                                             # Método inicializador da classe.
         self.textura = pygame.Surface(self.tamanho)                 # Criando um quandrado para a frutinha e atibuindo o tamanho da mesma.
         self.textura.fill(self.cor)                                 # Atribuindo a cor da variavel ao elemento.
 
@@ -23,7 +33,9 @@ class Frutinha:                                                     # Definindo 
         y = random.randint(0, 49) * 10
         self.posicao = (x, y)                                       # Definindo a posição da frutinha.
 
+
 frutinha = Frutinha()
+cobrinha = Snake()
 
 
 
@@ -33,4 +45,7 @@ while True:
             exit()
 
     screen.blit(frutinha.textura, frutinha.posicao)                 # Desenhando a frutinha na tela na posição especificada.
+
+    for posicao in cobrinha.corpo:
+        screen.blit(cobrinha.textura, posicao)
     pygame.display.update()                                         # Atualizando a tela para exibir a frutinha.
